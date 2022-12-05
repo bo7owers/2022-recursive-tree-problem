2ae498a09fb31b8eab7194e1ec1eb2c3b852a07d
bo7owers <7orres.rene@proton.me>
2022-12-04 21:20:23 -0500

    Took me a while to figure it out but styled tree!

--- src/ChristmasTree.vue
+++ src/ChristmasTree.vue
@@ -6,24 +6,45 @@ interface Props {
 const props = withDefaults(defineProps<Props>(), {
   size: Number,
 })
-
-let dotsArray: number[] = []
-
-for (let i = 0; i < props.size; i++) {
-  dotsArray.push(i)
-  console.log(i)
-}
-
-console.log(dotsArray)
 </script>
 <template>
   <div>
+    <ChristmasTree v-if="size > 1" :size="size - 1" />
     <div class="flex flex-row justify-center">
       <!-- This div is styled for the tree sections, but it's not complete -->
       <div
-        class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center"
-        v-for="dot in dotsArray"
-      />
+        class="absolute bg-yellow-500 w-4 h-4 flex justify-center items-center z-50 top-5 star"
+        v-if="size === 1"
+      ></div>
+      <div
+        class="absolute bg-yellow-500 w-4 h-4 flex justify-center items-center z-50 top-5 star-inverted"
+        v-if="size === 1"
+      ></div>
+      <div class="relative rounded-full bg-green w-16 h-16 flex justify-center items-center" v-for="dot in size">
+        <div class="absolute rounded-full bg-red-500 w-4 h-4 flex justify-center items-center"></div>
+      </div>
     </div>
   </div>
+  <div class="log">
+    <div class="bg-orange-900 w-10 h-16" v-if="size === 7"></div>
+  </div>
 </template>
+
+<style scoped>
+.star {
+  scale: 1.5;
+  top: 12.5em;
+  rotate: 45deg;
+}
+.star-inverted {
+  scale: 1.5;
+  top: 12.5em;
+  rotate: 0;
+}
+
+.log {
+  position: relative;
+  top: 16em;
+  left: -15.25em;
+}
+</style>
